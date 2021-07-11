14c515e96abbf244875233777115d1fdaad67a6a
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-07-11 20:19:46 +0530

    min and max

---
+++ Maxmin.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Pair
+{
+    public:
+        int min;
+        int max;
+
+        void minMaxArray(int arr[], int n);
+};
+pair *pair::minMaxArray(int arr[], int n)
+{
+    pair minMax;
+    if(n==1)
+    {
+        minMax.min;
+        minMax.max;
+    }
+}
+int main()
+{
+    return 0;
+}
