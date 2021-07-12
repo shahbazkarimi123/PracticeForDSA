c9fcf928064f3f0314afa932211996fc0a9ca024
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-07-12 09:09:00 +0530

    setting synced and in maxmin tree

--- Maxmin.cpp
+++ Maxmin.cpp
@@ -1,24 +1,42 @@
 #include<iostream>
 using namespace std;
 
-class Pair
+class Node
 {
     public:
-        int min;
-        int max;
+        int data;
+        Node* left;
+        Node* right;
 
-        void minMaxArray(int arr[], int n);
+        Node(int val)
+        {
+            data=val;
+            left=NULL;
+            right=NULL;
+
+        }
 };
-pair *pair::minMaxArray(int arr[], int n)
+void preorder(Node* root)
 {
-    pair minMax;
-    if(n==1)
+    if(root==NULL)
     {
-        minMax.min;
-        minMax.max;
+        return;
     }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+
 }
+
 int main()
 {
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    preorder(root);
     return 0;
 }
